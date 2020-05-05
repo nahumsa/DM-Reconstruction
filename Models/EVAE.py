fd950f3013505a630ba2c86e946d0a151a8db55f
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2020-05-05 16:37:23 -0300

    Added Entropy VAE

---
+++ Models/EVAE.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+from tqdm.notebook import tqdm
+from Utils.QMetrics import trace_loss, r_entropy_loss
+from Models.TVAE import TraceVAE
+
+class EntropyVAE(TraceVAE):
+  def __init__(self,
+               original_dim,
+               intermediate_dim,
+               latent_dim,               
+               **kwargs):    
+
+    super(EntropyVAE, self).__init__(original_dim,
+                                   intermediate_dim,
+                                   latent_dim)
+    
+  def training_step(self, x, r_loss, beta):
+    """Training step for the VAE.
+  
+    Parameters
+    -------------------------------------------
+    x: Data.
+    VAE(tf.keras.Model): Variational Autoencoder model. 
+    optimizer(tf.keras.optimizer): Optimizer used.  
+    r_loss(float): Parameter controlling reconstruction loss.
+    beta(float): Parameter controlling the KL divergence.
+
+    Return:
+    Loss(float): Loss value of the training step.
+
+    """
+    with tf.GradientTape() as tape:
+      reconstructed = self(x)  # Compute input reconstruction.
+      # Compute loss.      
+      loss = - r_entropy_loss(x, reconstructed)
+      kl = sum(self.losses)
+      loss = r_loss * loss + beta*kl  
+    
+    # Update the weights of the VAE.
+    grads = tape.gradient(loss, self.trainable_weights)
+    self.optimizer.apply_gradients(zip(grads, self.trainable_weights))        
+    return loss
