8c12954442ff4635326ad203fd1990c7fa5e4697
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2020-08-12 13:39:06 -0300

    Fixed test typo

--- CreateMeasurementsErr.py
+++ CreateMeasurementsErr.py
@@ -1,18 +1,21 @@
 from Utils.Dataset import create_dataset_err
 from Utils.Dataset import create_x
 
+
 samples_train = 5000
-measurements_train, measurements_errors_train = create_dataset_err(samples_train)
+measurements_train, measurements_errors_train = create_dataset_err(samples_train,
+                                                                   theta=np.pi/6)
 
 samples_test = 3000
-measurements_test, measurements_errors_test = create_dataset_err(samples_test)
+measurements_test, measurements_errors_test = create_dataset_err(samples_test, 
+                                                                 theta=np.pi/6)
 
 #Transforming in an np.array
 X_train = np.array(create_x(measurements_errors_train))
 Y_train = np.array(create_x(measurements_train))
 
-X_train = np.array(create_x(measurements_errors_test))
-Y_train = np.array(create_x(measurements_test))
+X_test = np.array(create_x(measurements_errors_test))
+Y_test = np.array(create_x(measurements_test))
 
 np.savetxt('Data/Errors/X_train.txt', X_train)
 np.savetxt('Data/Errors/Y_train.txt', Y_train)
